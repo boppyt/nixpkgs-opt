63853a9162464d035ed5a8ef0481082e23364032
boppyt <71049646+boppyt@users.noreply.github.com>
2021-07-14 06:42:12 -0700

    Update default.nix

--- pkgs/iosevka-pure-bin/default.nix
+++ pkgs/iosevka-pure-bin/default.nix
@@ -1,7 +1,7 @@
 { lib, fetchFromGitHub }:
 
 let
-  version = "7.2.0";
+  version = "7.2.0-1";
 in
 fetchFromGitHub rec {
   name = "iosevka-pure-bin-${version}";
@@ -9,7 +9,7 @@ fetchFromGitHub rec {
   owner = "boppyt";
   repo = "iosevka-pure";
   rev = "9cdc0b881b701354d8a90a05f120eb411ee20771";
-  sha256 = "sha256-EX/PJfFDgYyd7PbhjIJSHqvw14g2zv0dcgWwEEYKEaU=";
+  sha256 = "sha256-lbE/igEjAyPKNeUkcSknMmar0lVi0fYesnmUvNY9QcQ=";
 
   postFetch = ''
     tar xzf $downloadedFile --strip=1
